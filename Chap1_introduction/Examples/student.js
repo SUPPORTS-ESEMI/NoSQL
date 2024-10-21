77b05a3cbcc4da0048282c5c02396323f9406acb
Antoine07 <antoine.lucsko@gmail.com>
2024-10-21 11:24:08 +0200

    students

---
+++ Chap1_introduction/Examples/student.js
@@ -0,0 +1,123 @@
+[
+    {
+      "_id": 1,
+      "name": "Alan",
+      "address": {
+        "street": "Baker Street",
+        "city": "London",
+        "zip": "NW1"
+      },
+      "grade": "master 5",
+      "notes": [14, 17, 19, 20],
+      "relationship": null
+    },
+    {
+      "_id": 2,
+      "name": "Alice",
+      "address": {
+        "street": "Champs-Élysées",
+        "city": "Paris",
+        "zip": "75008"
+      },
+      "grade": "master 4",
+      "notes": [19, 11, 20],
+      "relationship": [1]
+    },
+    {
+      "_id": 3,
+      "name": "John",
+      "address": {
+        "street": "Main Street",
+        "city": "New York",
+        "zip": "10001"
+      },
+      "grade": "bachelor 3",
+      "notes": [12, 16, 14],
+      "relationship": [2, 4]
+    },
+    {
+      "_id": 4,
+      "name": "Emma",
+      "address": {
+        "street": "Rue de Rivoli",
+        "city": "Paris",
+        "zip": "75001"
+      },
+      "grade": "master 1",
+      "notes": [18, 15, 20],
+      "relationship": null
+    },
+    {
+      "_id": 5,
+      "name": "Michael",
+      "address": {
+        "street": "Wall Street",
+        "city": "New York",
+        "zip": "10005"
+      },
+      "grade": "master 2",
+      "notes": [13, 16, 12],
+      "relationship": [3]
+    },
+    {
+      "_id": 6,
+      "name": "Sophia",
+      "address": {
+        "street": "Gran Vía",
+        "city": "Madrid",
+        "zip": "28013"
+      },
+      "grade": "bachelor 2",
+      "notes": [15, 12, 16],
+      "relationship": null
+    },
+    {
+      "_id": 7,
+      "name": "Lucas",
+      "address": {
+        "street": "Alexanderplatz",
+        "city": "Berlin",
+        "zip": "10178"
+      },
+      "grade": "master 3",
+      "notes": [17, 19, 14],
+      "relationship": [4, 6]
+    },
+    {
+      "_id": 8,
+      "name": "Olivia",
+      "address": {
+        "street": "Via Condotti",
+        "city": "Rome",
+        "zip": "00187"
+      },
+      "grade": "bachelor 1",
+      "notes": [16, 14, 15],
+      "relationship": null
+    },
+    {
+      "_id": 9,
+      "name": "David",
+      "address": {
+        "street": "Rue de la Paix",
+        "city": "Paris",
+        "zip": "75002"
+      },
+      "grade": "master 5",
+      "notes": [18, 17, 19],
+      "relationship": [7, 8]
+    },
+    {
+      "_id": 10,
+      "name": "Sophia",
+      "address": {
+        "street": "Oxford Street",
+        "city": "London",
+        "zip": "W1D"
+      },
+      "grade": "bachelor 3",
+      "notes": [12, 18, 20],
+      "relationship": [5]
+    }
+  ]
+  
