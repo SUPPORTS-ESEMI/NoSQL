4814d00c2b1595e82f81dc9417e87868a8044da3
Antoine07 <antoine.lucsko@gmail.com>
2024-10-21 17:52:24 +0200

    exercice

--- Chap1_introduction/Exercices/01_exercices_find.js
+++ Chap1_introduction/Exercices/01_exercices_find.js
@@ -30,3 +30,16 @@ db.restaurants.find({
 }, {
     _id: 0, name: 1, "grades.score": 1
 }).count()
+
+//  Quels sont les restaurants qui ont eu un grade A et un score supérieur ou égal à 20
+
+db.restaurants.find({
+    $and: [
+        { "grades.grade": "A" },
+        { "grades.score": { $gte: 20 } }
+    ]
+}, {
+    _id: 0, name: 1, "grades.score": 1, "grades.grade" : 1
+}).sort({
+    name : 1
+})
